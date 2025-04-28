ee73d43f0b03863d60826bfe4674f49eb030f167
Michael T <ion.tck@gmail.com>
2025-04-28 13:43:18 -0500

    update Sequelize to use connection string

--- database/connection.js
+++ database/connection.js
@@ -1,14 +1,6 @@
 import Sequelize from "sequelize";
 import "dotenv/config";
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "postgres",
-  },
-);
+const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING);
 
 export default sequelize;
