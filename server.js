d61e78dbf8b29d0bb32991df199adfcc41b93754
Michael T <ion.tck@gmail.com>
2025-04-28 22:40:58 -0500

    add Procfile and enhance server.js with shutdown handling and default port

--- server.js
+++ server.js
@@ -6,7 +6,23 @@ import sequelize from "./database/connection.js";
 import "./models/project.model.js";
 import "./models/task.model.js";
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3809;
+
+process
+  .on("SIGTERM", shutdown("SIGTERM"))
+  .on("SIGINT", shutdown("SIGINT"))
+  .on("uncaughtException", shutdown("uncaughtException"));
+
+function shutdown(signal) {
+  return (err) => {
+    console.log(`${signal}...`);
+    if (err) console.error(err.stack || err);
+    setTimeout(() => {
+      console.log("...waited 5s, exiting.");
+      process.exit(err ? 1 : 0);
+    }, 5000).unref();
+  };
+}
 
 // Initializes the server
 const main = async () => {
